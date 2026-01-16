7670d1f317af986991cd23ad5cff3b7733f8a124
Rayan-ouer <rayan.ouerdane@epitech.eu>
2026-01-16 13:57:08 +0100

    feat(app): update config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://project_week_cyber.github.io',
+  base: '/project_week',
   integrations: [react()],
 
   vite: {
